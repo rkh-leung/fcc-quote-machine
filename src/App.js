8c967b60ad7763e4e502f05682382e3aa2207035
rkh-leung <leptanian@gmail.com>
2021-07-29 11:31:05 +1200

    add: font awesome twitter icon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Random Quote Machine</h1>
-            </header>
-            <section id='quote-box'>
-                <p id='text'>Quote goes here</p>
-                <p id='author'>Author</p>
-                <button id='new-quote'>New quote</button>
-            </section>
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Random Quote Machine</h1>
+      </header>
+      <section id='quote-box'>
+        <p id='text'>Quote goes here</p>
+        <p id='author'>Author</p>
+        <div className='div'>
+          <a id='tweet-quote' target='_top'>
+            <i className='fab fa-twitter'></i>
+          </a>
+          <button id='new-quote'>New quote</button>
         </div>
-    );
+      </section>
+    </div>
+  )
 }
 
-export default App;
+export default App
