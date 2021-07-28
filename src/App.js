838b307c0fbb2f661b9d3c6ed82c0c8235fed9ac
rkh-leung <leptanian@gmail.com>
2021-07-29 10:19:49 +1200

    add: prettier config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Random Quote Machine</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Random Quote Machine</h1>
+            </header>
+            <section id='quote-box'>
+
+            </section>
+        </div>
+    );
 }
 
 export default App;
